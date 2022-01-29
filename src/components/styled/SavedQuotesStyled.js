bbd534caf1a8dfc28a2a182e8ceef9be5b70f01e
Minrey <minreypeng@gmail.com>
2022-01-29 22:44:40 +0800

    Small Tweaks

--- src/components/styled/SavedQuotesStyled.js
+++ src/components/styled/SavedQuotesStyled.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const GridContainer = styled.main`
     width: 100%;
+    height: 100%;
+    min-height: 100vh;
     max-width: 960px;
     margin: 0 auto;
     padding: 2rem 1rem;
